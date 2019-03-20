e95c155f3059de4b8532280ab9db81d9d6042ce7
MisfitLove <30420775+MisfitLove@users.noreply.github.com>
2019-03-20 19:05:19 +0100

    skonczone?

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ const mysql = require('mysql');
 
 var server = restify.createServer();
 
+server.use(restify.plugins.bodyParser());
+
 server.get('/', function (req, res, next) {
     let json = JSON.stringify([{ name: "baza 1" }, { name: 'baza 2' }]);
     res.send(json);
@@ -63,10 +65,38 @@ server.get('/db', function (req, res, next) {
 
 server.post('/query', (req, res) => {
     console.log(req.body.username);
-    res.send(200, 'ok');
+    connect(req.body, res1 => res.send(200, res1));
 });
 
 server.listen(3000, function() {
   console.log('%s listening at %s', server.name, server.url);
 
 });
+
+function connect(body, callback){
+    var connection = mysql.createConnection({
+        host     : 'localhost',  //body.host
+        database : 'bank',       //body.db
+        user     : 'root',       //body.user
+        password : '',           //body.password
+    });
+
+    connection.connect(function (err) {
+        if (err) {
+            console.error('Error connecting: ' + err.stack);
+            return;
+        }
+
+        console.log('Connected as id ' + connection.threadId);
+
+    });
+
+    connection.query('SHOW DATABASES;', function (error, results, fields) {
+        if (error)
+            throw error;
+
+        callback(JSON.stringify(results));
+    });
+    connection.end();
+    
+};
